9226bd560f2d4c578f3b903dd6a1a2cc73956cc8
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-02-10 19:54:07 -0300

    Project uploaded

---
+++ models/author.js
@@ -0,0 +1,39 @@
+var mongoose = require('mongoose');
+const { DateTime } = require("luxon");
+
+var Schema = mongoose.Schema;
+
+var AuthorSchema = new Schema(
+  {
+    first_name: {type: String, required: true, maxlength: 100},
+    family_name: {type: String, required: true, maxlength: 100},
+    date_of_birth: {type: Date},
+    date_of_death: {type: Date},
+  }
+);
+
+// Virtual for author's full name
+AuthorSchema
+.virtual('name')
+.get(function () {
+  return this.family_name + ', ' + this.first_name;
+});
+
+// Virtual for author's lifespan
+AuthorSchema
+.virtual('lifespan')
+.get(function () {
+  let date_of_birth_formatted = (this.date_of_birth) ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
+  let date_of_death_formatted = (this.date_of_death) ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
+  return `${date_of_birth_formatted} - ${date_of_death_formatted}`;
+});
+
+// Virtual for author's URL
+AuthorSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/author/' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('Author', AuthorSchema);
