1f2badb24dbcb68b4facf7e4b05aeeac14e97fcb
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-02-11 16:19:39 -0300

    Minor changes

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var dev_db_url = 'mongodb+srv://sebaasdf:s190399s@cluster0.krmke.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://cooluser:coolpassword@cluster0.krmke.mongodb.net/local_library?retryWrites=true&w=majority';
 var mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
